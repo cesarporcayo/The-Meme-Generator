f8640dca34bf29eb0d0474560b0122bee3c563ba
Cesar Porcayo <cesarporcayo@gatech.edu>
2022-07-15 10:26:33 -0700

    first commit

---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+import React from "react"
+import style from "../style.css"
+
+export default function Header() {
+    return (
+        <header className="header">
+            <div className="title--header">
+                <h2>Meme</h2>
+                <h2>Generator</h2>
+            </div>
+            
+        </header>
+    )
+}
